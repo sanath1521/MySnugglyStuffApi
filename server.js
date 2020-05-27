2f8fc423dd714368956e7dcd283d158f681fcbfa
pradmuni <saipradeep40@gmail.com>
2020-05-27 10:46:05 +0530

    body parser limit bug

--- server.js
+++ server.js
@@ -17,8 +17,15 @@ mongoose
     { useNewUrlParser: true, useUnifiedTopology: true },
   )
   .then(() => console.log("Connected to db"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
+
 app.use(cookieParer())
 
 
